c01c044bb15f519683d7738ba69148f34994e4e8
bruno <bruno.balbuena2112@gmail.com>
2021-04-19 17:03:59 -0500

    Principio de props

--- first-project/src/components/Expenses/ExpenseItem.js
+++ first-project/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const [title_, setTitle] = useState(title);
+  const clicked = () => {
+    console.log("Clicked");
+    setTitle("Updated!");
+  };
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <div>
         <ExpenseDate date={date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{title_}</h2>
         <div className="expense-item__price">{amount}</div>
+        <button onClick={clicked}>Click Here</button>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- first-project/src/components/Expenses/Expenses.js
+++ first-project/src/components/Expenses/Expenses.js
@@ -1,18 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 import "./Expenses.css";
 const Expenses = ({ expenses }) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {expenses.map((expense) => {
         return (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             date={expense.date}
             amount={expense.amount}
           ></ExpenseItem>
         );
       })}
-    </div>
+    </Card>
   );
 };
 
