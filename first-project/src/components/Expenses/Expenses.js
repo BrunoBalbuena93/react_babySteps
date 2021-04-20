8846f4bb51ac917b84399a9f73d0f292d0a9fcd4
bruno <bruno.balbuena2112@gmail.com>
2021-04-20 17:41:58 -0500

    Props hacia arriba

--- first-project/src/components/Expenses/Expenses.js
+++ first-project/src/components/Expenses/Expenses.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 const Expenses = ({ expenses }) => {
+  const [filterDate, setFilterDate] = useState("2019");
+
+  const changeDate = (selectedYear) => {
+    // setFilterDate(e.target.value);
+    console.log(selectedYear);
+    setFilterDate(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        onChangeDate={changeDate}
+        initialYear={filterDate}
+      ></ExpenseFilter>
       {expenses.map((expense) => {
         return (
           <ExpenseItem
