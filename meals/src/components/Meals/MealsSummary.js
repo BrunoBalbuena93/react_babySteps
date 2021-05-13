b64b1c3b3ee1d360b997dfedc4fd534daaf99cb6
bruno <bruno.balbuena2112@gmail.com>
2021-05-13 01:20:55 -0500

    Starting the projects

---
+++ meals/src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './MealsSummary.module.css'
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious Food mate</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis possimus, dolor adipisci praesentium assumenda veniam dolorem alias voluptatem quos omnis itaque, similique corporis maiores officiis ea tenetur mollitia aliquam. Ut?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure sed ea repudiandae magni? Sit, accusamus, sed, vero quo distinctio quas et iure dicta porro id qui facilis optio veniam nemo.</p>
+        </section>
+    )
+}
+
+export default MealsSummary
