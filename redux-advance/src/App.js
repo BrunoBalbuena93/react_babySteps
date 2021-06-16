856eee913f3bf706143ad51eccd72a973e0336c3
bruno <bruno.balbuena2112@gmail.com>
2021-06-16 00:41:22 -0500

    redux mid 2

--- redux-advance/src/App.js
+++ redux-advance/src/App.js
@@ -1,10 +1,46 @@
-import {useSelector} from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "pending...",
+        message: "Sending cart data",
+      })
+    );
+    fetch("https://angular-course-85ad3.firebaseio.com/redux-mid/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    })
+      .then((response) => {
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Sucess!",
+            message: "Data stored correctly",
+          })
+        );
+      })
+      .catch((err) => {
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error",
+            message: "Something wrong happened",
+          })
+        );
+      });
+  }, [cart, dispatch]);
 
   return (
     <Layout>
