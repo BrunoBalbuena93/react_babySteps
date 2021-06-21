0b94674c4256e078c0b6d308887079a0b1c7fc22
bruno <bruno.balbuena2112@gmail.com>
2021-06-21 08:23:36 -0500

    router |

---
+++ router/src/App.js
@@ -0,0 +1,27 @@
+import {Route} from 'react-router-dom';
+import Welcome from './pages/Welcome'
+import Products from './pages/Products'
+import MainHeader from './components/MainHeader';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <MainHeader></MainHeader>
+      </header>
+      <main>
+        <Route path='/welcome'>
+          <Welcome></Welcome>
+        </Route>
+        <Route path='/products'>
+          <Products></Products>
+        </Route>
+        <Route>
+          
+        </Route>
+      </main>
+    </div>
+  );
+}
+
+export default App;
